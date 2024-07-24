b8c2e4b8e7e43bff04ab981609f819edf5ef2e47
Pascal512 <pascaldegla512@gmail.com>
2024-07-24 07:20:39 +0100

    Ajout de la page Onboarding1

---
+++ src/components/HeaderOnboarding.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="w-100 px-5 pt-3 position-absolute top-0 start-0">
+        <div class="col-12 col-md-6 mx-auto">
+            <div class="row justify-content-between">
+                <div class="col-auto p-0 pt-2 align-items-center">
+                    <!-- p class="point" :class="{'point-actif': pointActif=='1'}" ></p -->
+                    <router-link to="/Onboarding1">
+                        <p class="point" :class="{'point-actif': pointActif=='1'}"></p>
+                    </router-link>
+                    <router-link to="/Onboarding2">
+                        <p class="point" :class="{'point-actif': pointActif=='2'}"></p>
+                    </router-link>
+                    <router-link to="/Onboarding3">
+                        <p class="point" :class="{'point-actif': pointActif=='3'}"></p>
+                    </router-link>
+                    <router-link to="/Onboarding4">
+                        <p class="point" :class="{'point-actif': pointActif=='4'}"></p>
+                    </router-link>
+                </div>
+                <div class="col-auto p-0">
+                    <p class="decoration-none fs-6 fw-bold d-inline-block">
+                        <router-link to="/OnboardingFin">Passer</router-link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderOnboarding',
+    props: {
+        pointActif: String
+    }
+}
+</script>
+
+<style scoped>
+.point {
+    display: inline-block;
+    background-color: black;
+    width: 5px;
+    height: 5px;
+    margin-right: 5px;
+    border-radius: 3px;
+    align-self: center;
+}
+.point-actif {
+    width: 35px;
+}
+.decoration-none a {
+    text-decoration: none !important;
+    color: black;
+}
+</style>
--- src/views/Onboarding1.vue
+++ src/views/Onboarding1.vue
@@ -1,9 +1,40 @@
 <template>
-    <img src="../assets/Image1.png" alt="">
+    <div class="h-100 w-100">
+        <HeaderOnboarding pointActif="1" />
+        <div class="mx-auto col-12 col-md-5 px-5 pt-5 h-45vh align-items-center">
+            <img src="../assets/Image1.png" alt="" class="w-100 mt-5" >
+        </div>
+
+        <div class="col-12 col-md-6 mx-auto bg-sombre px-5 pt-5 conteneur-sombre text-center h-55vh">
+            <h2 class="w-75 mx-auto fw-900 pt-md-5">Surveillez en Temps Réel</h2>
+            <p class="pt-3">Visualisez les flux vidéo de vos caméras de surveillance en direct et gardez un œil sur votre maison où que vous soyez.</p>
+            <router-link to="/Onboarding2">
+                <div class="bouton bouton-clair d-inline-block mt-3">Continuer</div>
+            </router-link>
+        </div>
+    </div>
 </template>
 
 <script>
+import HeaderOnboarding from '@/components/HeaderOnboarding.vue';
+
 export default {
-    name: 'OnboardingFirst'
+    name: 'OnboardingFirst',
+    components: {
+        HeaderOnboarding
+    }
 }
 </script>
+
+<style scoped>
+.conteneur-sombre {
+    border-top-left-radius: 40px;
+    border-top-right-radius: 40px;
+}
+.h-45vh {
+    height: 45vh !important;
+}
+.h-55vh {
+    height: 55vh !important;
+}
+</style>
